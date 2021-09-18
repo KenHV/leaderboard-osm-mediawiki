34be552d2536fbd44d0d0d2618572d98e8fa4a88
KenHV <yo@kenharris.xyz>
2021-09-18 17:20:27 +0530

    Add basic API wrapper for MediaWiki
    Change-Id: Iadf37962c82dd31d9f8395fa704341cba6a7e12c
    Signed-off-by: KenHV <yo@kenharris.xyz>

---
+++ mw_api_wrapper.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import aiohttp
+
+
+async def get_response(user: str) -> str:
+    url = "https://www.mediawiki.org/w/api.php"
+    params = {
+        "action": "query",
+        "format": "json",
+        "list": "users",
+        "ususers": user,
+        "usprop": "editcount",
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as response:
+            json = await response.json()
+    return json
+
+
+async def main():
+    user = input("Enter username: ")
+    json = await get_response(user)
+    score = json["query"]["users"][0]["editcount"]  # type: ignore
+
+    print(f"User: {user} | Score: {score}")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
